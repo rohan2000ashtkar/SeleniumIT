c879f393d0ffff5f8bf5c98f5bb8117a3eafbabb
rohan2000ashtkar <rohan2000rohith@gmail.com>
2025-03-14 14:31:25 +0530

    day9

---
+++ src/day9/Navigationcmds.java
@@ -0,0 +1,24 @@
+package day9;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Navigationcmds {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.com/");
+		Thread.sleep(2000);
+		driver.navigate().to("https://www.flipkart.com/");
+		Thread.sleep(2000);
+		driver.navigate().back();
+		Thread.sleep(2000);
+		driver.navigate().forward();
+		Thread.sleep(2000);
+		driver.navigate().refresh();
+
+	}
+
+}
